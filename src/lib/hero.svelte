f17e3fe8e9eaf551e75051b12df75fc58810326d
GamuWasTaken <victordiezruiz@proton.me>
2026-04-01 19:36:52 +0200

    feat: hero start

---
+++ src/lib/hero.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+  import heroImg from "$lib/assets/1237.jpg";
+</script>
+
+<div class="hero" style:--bgimg={`url(${heroImg})`}>
+  <img src={heroImg} alt="" />
+
+  <div class="title">
+    <h2>Manejo del <br /> <span>Instinto</span></h2>
+    <h6>
+      Infraestructura ganadera que entiende al animal. Sin adornos, solo acero
+      forjado en la montaña.
+    </h6>
+  </div>
+</div>
+
+<style>
+  .hero {
+    height: 100vh;
+    position: relative;
+    display: grid;
+    grid-template-columns: 100px auto 1fr;
+    grid-template-rows: 1fr auto 100px;
+    background: linear-gradient(15deg, rgba(0, 0, 0, 0.9), transparent);
+  }
+
+  img {
+    position: absolute;
+    object-fit: cover;
+    object-position: right top;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    z-index: -1;
+  }
+
+  .title {
+    grid-column: 2/3;
+    grid-row: 2/3;
+
+    h2 {
+      font-size: 6em;
+      font-family: Newsreader;
+      color: var(--bg);
+      margin: 0;
+    }
+    h2 span {
+      color: var(--yellow);
+    }
+
+    h6 {
+      margin: 0;
+      display: block;
+      width: 400px;
+      font-size: 1em;
+      font-family: sans-serif;
+      color: var(--bg);
+
+      border-left: 3px solid var(--yellow);
+      padding-left: 10px;
+    }
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,39 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import Hero from '$lib/hero.svelte'
+  //946 951 997 1000 1021 1230 1237
+</script>
+
+<header>Lanchares Agrícola</header>
+
+<Hero/>
+
+<div>Cancilla a la carta</div>
+
+<div>Contacto</div>
+
+<footer></footer>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Newsreader:ital,opsz,wght@0,6..72,200..800;1,6..72,200..800&display=swap');
+
+  :global(:root) {
+    --yellow: #efbf03;
+    --red: #770506;
+    --bg: #f4eedf;
+    --fg: #1b1c18;
+  }
+
+  :global(body) {
+    margin: 0;
+  }
+
+  header {
+    position: fixed;
+    width: 100vw;
+    height: 60px;
+    background-color: var(--bg);
+
+    display: none;
+  }
+
+</style>
